afe8a92744b45e70b7bd5285584bfab532d7f866
Vukašin Lekić <goksi1312@gmail.com>
2022-12-27 02:27:03 +0100

    Update Listener.java

--- src/main/java/tech/goksi/remoteconsole/events/Listener.java
+++ src/main/java/tech/goksi/remoteconsole/events/Listener.java
@@ -1,13 +1,31 @@
 package tech.goksi.remoteconsole.events;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import tech.goksi.remoteconsole.RemoteConsole;
+import tech.goksi.remoteconsole.api.exceptions.WebSocketException;
+import tech.goksi.remoteconsole.api.models.ConsoleUser;
 import tech.goksi.remoteconsole.api.models.events.AuthEvent;
 import tech.goksi.remoteconsole.api.models.events.CommandSendEvent;
 import tech.goksi.remoteconsole.events.hooks.ListenerAdapter;
+import tech.goksi.remoteconsole.token.JWTParser;
 
 public class Listener extends ListenerAdapter {
+    /*TODO token expiring, bukkit scheduler ig, and add token expired event*/
     @Override
     public void onAuthEvent(AuthEvent event) {
-        System.out.println(event.getJWToken());
+        DecodedJWT decodedJWT;
+        try {
+            decodedJWT = JWTParser.parse(event.getJWToken());
+        } catch (JWTVerificationException exception) {
+            event.getContext().send(new WebSocketException("TokenException", "Provided JWT is invalid"));
+            return;
+        }
+        String username = decodedJWT.getClaim("user").asString();
+        RemoteConsole.getInstance()
+                .getWebsocketHandler()
+                .addObserver(new ConsoleUser(username, event.getContext()));
+        event.getContext().send("{\"event\": \"AuthSuccess\"}");
     }
 
     @Override
