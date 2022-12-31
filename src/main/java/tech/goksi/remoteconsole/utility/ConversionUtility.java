549d946433432a2c0684ae96af7b5de8d1a4ae00
Vukašin Lekić <goksi1312@gmail.com>
2022-12-31 02:10:04 +0100

    Create ConversionUtility.java

---
+++ src/main/java/tech/goksi/remoteconsole/utility/ConversionUtility.java
@@ -0,0 +1,14 @@
+package tech.goksi.remoteconsole.utility;
+
+public class ConversionUtility {
+    private ConversionUtility() {
+    }
+
+    public static long minutesToTicks(int minutes) {
+        return (long) minutes * 60 * 20;
+    }
+
+    public static int millisToMinutes(long millis) {
+        return (int) (millis / 1000);
+    }
+}
