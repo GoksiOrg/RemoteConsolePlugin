02a7cbaaf2612c1efc7b2c2d4bd5d56dc4da5d13
Vukašin Lekić <goksi1312@gmail.com>
2023-01-06 11:55:38 +0100

    Adapting errors

--- src/main/java/tech/goksi/remoteconsole/api/models/events/ConsoleOutputEvent.java
+++ src/main/java/tech/goksi/remoteconsole/api/models/events/ConsoleOutputEvent.java
@@ -1,23 +1,30 @@
 package tech.goksi.remoteconsole.api.models.events;
 
-import org.apache.logging.log4j.Level;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.logging.log4j.core.LogEvent;
 
 import java.util.Arrays;
 
-/*TODO check exception, thrown*/
 public class ConsoleOutputEvent extends GenericEvent {
     private final long timestamp;
     private final String message;
-    private final Level logLevel;
+    private final String logLevel;
     private final String loggerName;
-    /*TODO level empty, check*/
+    private final String stackTrace;
+
     public ConsoleOutputEvent(LogEvent logEvent) {
-        super("console_output", Arrays.asList(logEvent.getTimeMillis(), logEvent.getLevel(), logEvent.getLoggerName(), logEvent.getMessage().getFormattedMessage()));
+        super("console_output", Arrays.asList(
+                logEvent.getTimeMillis(),
+                logEvent.getLevel().toString(),
+                logEvent.getLoggerName(),
+                logEvent.getMessage().getFormattedMessage(),
+                logEvent.getThrown() == null ? null : ExceptionUtils.getStackTrace(logEvent.getThrown())
+        ));
         this.timestamp = (long) getData().get(0);
-        this.logLevel = (Level) getData().get(1);
+        this.logLevel = (String) getData().get(1);
         this.loggerName = (String) getData().get(2);
         this.message = (String) getData().get(3);
+        this.stackTrace = (String) getData().get(4);
     }
 
     public long getTimestamp() {
@@ -28,11 +35,15 @@ public class ConsoleOutputEvent extends GenericEvent {
         return message;
     }
 
-    public Level getLogLevel() {
+    public String getLogLevel() {
         return logLevel;
     }
 
     public String getLoggerName() {
         return loggerName;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
     }
 }
