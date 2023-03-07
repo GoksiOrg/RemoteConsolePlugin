5af352a4e26cb0f1007369a3d5a473658364a821
Vukašin Lekić <goksi1312@gmail.com>
2023-03-07 01:43:01 +0100

    Create CommonUtility.java

---
+++ src/main/java/tech/goksi/tabbycontrol/utility/CommonUtility.java
@@ -0,0 +1,17 @@
+package tech.goksi.tabbycontrol.utility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonUtility {
+
+    public static Map<String, Object> mapOf(Object... objects) {
+        Map<String, Object> map = new HashMap<>();
+        if (objects.length % 2 != 0) throw new IllegalArgumentException("Must have even number of arguments");
+        for (int i = 0; i < objects.length; i += 2) {
+            if (!(objects[i] instanceof String)) throw new IllegalArgumentException("Key must be string !");
+            map.put((String) objects[i], objects[i + 1]);
+        }
+        return map;
+    }
+}
