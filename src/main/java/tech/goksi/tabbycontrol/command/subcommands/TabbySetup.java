d8fd59cf32de2b9a58cad0afbc529cff2caed181
Vukašin Lekić <goksi1312@gmail.com>
2023-04-08 13:51:49 +0200

    Adding ssl arg

--- src/main/java/tech/goksi/tabbycontrol/command/subcommands/TabbySetup.java
+++ src/main/java/tech/goksi/tabbycontrol/command/subcommands/TabbySetup.java
@@ -1,10 +1,12 @@
 package tech.goksi.tabbycontrol.command.subcommands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import tech.goksi.tabbycontrol.TabbyControl;
 import tech.goksi.tabbycontrol.command.CommandHandler;
+import tech.goksi.tabbycontrol.utility.ConversionUtility;
 
 import static tech.goksi.tabbycontrol.utility.CommonUtility.sendMessage;
 
@@ -15,7 +17,7 @@ public class TabbySetup implements CommandHandler {
             sendMessage(sender, "OnlyConsole");
             return;
         }
-        if (args.length != 5) {
+        if (args.length != 6) {
             sendMessage(sender, "WrongUsage");
             return;
         }
@@ -23,8 +25,11 @@ public class TabbySetup implements CommandHandler {
         config.set("ConsoleConfiguration.ID", args[1]);
         config.set("ConsoleConfiguration.Port", Integer.parseInt(args[2]));
         config.set("ConsoleConfiguration.Remote", args[3]);
-        config.set("ConsoleConfiguration.Secret", args[4]);
+        config.set("ConsoleConfiguration.SSL.Enabled", Boolean.valueOf(args[4]));
+        config.set("ConsoleConfiguration.Secret", args[5]);
         TabbyControl.getInstance().saveConfig();
         sendMessage(sender, "SuccessSetup");
+        Bukkit.getScheduler()
+                .runTaskLater(TabbyControl.getInstance(), Bukkit.spigot()::restart, ConversionUtility.secondsToTicks(3));
     }
 }
